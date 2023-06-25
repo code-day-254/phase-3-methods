89fb7c04f0809225fbc05730d48ccff66da9ce74
Moffat <moffat.muga@gmail.com>
2023-06-25 15:15:54 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default 
+greet_with_default "Sunny"
+
+def add_and_return(num1, num2)
+    return num1 + num2
+end
+sum1 = add_and_return(2, 5)
+  
+def halve_and_return(num1, num2)
+    return num1 / num2
+end
+halve_and_return(6, 2)
+
+def halve_and_log(num1, num2)
+    puts num1 / num2
+end
+halve_and_log(6, 2)
